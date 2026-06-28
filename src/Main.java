528c1102ecf4f1814decc3c7e3c415c824713108
killuao0 <yashrajsingh111008@gmail.com>
2026-06-28 20:18:46 +0530

    Main

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,23 @@ public class Main {
     public static void main(String[] args) {
         Random ran = new Random();
         int number = ran.nextInt(100);
+        System.out.println(number);
         Scanner sc = new Scanner(System.in);
         System.out.println("enter your number");
         int guess;
+        int num= 1;
         guess = sc.nextInt();
         while (guess!=number){
+            num++;
             if (guess<number){
                 System.out.println("guess a higher number");
             } else {
                 System.out.println("guess a lower number");
             }
+            System.out.println("enter your next guess number " + num);
             guess = sc.nextInt();
         }
-        System.out.println("ta-da you got it. CONGRATULATIONS");
+        System.out.println("ta-da you got it. CONGRATULATIONS." +
+                " you got it in "+ num + " tries");
     }
 }
